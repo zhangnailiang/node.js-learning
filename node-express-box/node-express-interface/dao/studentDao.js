88b70e0d99362ff528004806e7c9e8aacb4e266f
xiaohai <xiaohai@dtstack.com>
2019-10-16 17:12:03 +0800

    向数据库添加数据

--- node-express-box/node-express-interface/dao/studentDao.js
+++ node-express-box/node-express-interface/dao/studentDao.js
@@ -13,6 +13,23 @@ function queryAllStudent(success) {
     });
     connection.end();
 }
+function insertStudent(id, stuNum, name, age, stuClass, math, pwd, success) {
+    var insertSql = "insert into student(id, stu_num, name, age, class,math, pwd) values (?,?,?,?,?,?,?)";
+    var params = [id, stuNum, name, age, stuClass, math, pwd];
+    console.log("**params**", params);
+    var connection = doutil.createConnection();
+    connection.connect();
+    connection.query(insertSql, params, function (error, result) {
+        if (error == null) {
+            console.log(result);
+            success(result);
+        } else {
+            throw new Error(error)
+        }
+    });
+    connection.end();
+}
 module.exports = {
-    "queryAllStudent": queryAllStudent
+    "queryAllStudent": queryAllStudent,
+    "insertStudent": insertStudent
 }
--- node-express-box/node-express-interface/web/studentController.js
+++ node-express-box/node-express-interface/web/studentController.js
@@ -1,4 +1,5 @@
 var studentDao = require("../dao/studentDao");
+var url = require("url");
 var path = new Map();
 function getAllStudent(request, response) {
     studentDao.queryAllStudent(function (result) {
@@ -7,5 +8,19 @@ function getAllStudent(request, response) {
         response.end();
     })
 }
+function insertStudent(request, response) {
+    var params = url.parse(request.url, true).query;
+    console.log("**params1**", params);
+    // studentDao.insertStudent([...params])
+    studentDao.insertStudent(params.id, params.stuNum, params.name, params.age, params.stuClass, params.math, params.pwd, function () {
+        response.writeHead(200, { "Content-Type": "text/html;charset=utf-8" })
+        response.write("添加成功");
+        response.end
+    })
+
+
+}
 path.set("/queryAllStudent", getAllStudent);
+path.set("/insertStudent", insertStudent);
+
 module.exports.path = path;
